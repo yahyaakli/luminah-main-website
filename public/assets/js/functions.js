9cbffd72fd5247139ce63aa3ef576ea1b12b891b
Yahya Akli <yahyaakli@Yahyas-MBP.lan>
2025-01-23 15:13:27 -0500

    Add assets

---
+++ public/assets/js/functions.js
@@ -0,0 +1,327 @@
+/*================================================
+*
+* Template name : Mone
+* Version       : 1.0.1
+* Author        : FlaTheme
+* Author URL    : http://themeforest.net/user/flatheme
+*
+* Table of Contents :
+* 1. Page Preloader
+* 2. Cursor
+* 3. Header Nav Menu
+* 4. Scroll To Top
+* 5. Sliders
+* 6. Lightbox
+* 7. Google Maps
+* 8. Contact Form
+*
+================================================*/
+"use strict";
+
+var $body = $("body");
+
+/*===============================================
+  1. Page Preloader
+===============================================*/
+$(window).on("load", function () {
+  $body.addClass("loaded");
+});
+
+if ($body.attr("data-preloader") === "true") {
+  $body.append($("<div class='preloader'><div><span>L</span><span>O</span><span>A</span><span>D</span><span>I</span><span>N</span><span>G</span></div></div>"));
+}
+
+/*===============================================
+  2. Cursor
+===============================================*/
+var customCursor = document.getElementById("cursor");
+
+if (customCursor) {
+  var cursor = document.getElementById("cursor");
+  document.addEventListener('mousemove', function(e) {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+  });
+
+  var mouseElms = document.querySelectorAll("a, button, input, textarea, .cursor-link");
+
+  mouseElms.forEach(function(mouseElm) {
+    mouseElm.addEventListener("mouseenter", function() {
+      cursor.classList.add("scale-cursor");
+    });
+    mouseElm.addEventListener("mouseleave", function() {
+      cursor.classList.remove("scale-cursor");
+    });
+  });
+}
+
+
+/*===============================================
+  3. Header Nav Menu
+===============================================*/
+var headerNav = $(".nav-box");
+
+if (headerNav.length) {
+  var toggleBtn = $("#nav-toggle");
+  //
+  // Menu Toggle //
+  //
+  toggleBtn.on("click", function() {
+    if (headerNav.hasClass("show")) {
+      headerNav.removeClass("show");
+      toggleBtn.removeClass("active");
+    }
+    else {
+      headerNav.addClass("show");
+      toggleBtn.addClass("active");
+    }
+  });
+  //
+  // Close Menu //
+  //
+  $(document).on("click", function(e) {
+    if ( $(e.target).closest(".nav-box, #nav-toggle").length === 0 ) {
+      if (headerNav.hasClass("show")) {
+        headerNav.removeClass("show");
+        toggleBtn.removeClass("active");
+      }
+    }
+  });
+}
+
+/*===============================================
+  4. Scroll To Top
+===============================================*/
+var scrollTopBtn = document.querySelector(".scrolltotop");
+
+if (scrollTopBtn) {
+  // Show, Hide //
+  window.addEventListener("scroll", function() {
+    if (window.pageYOffset > 700) { // 700px from top
+      scrollTopBtn.classList.add("show");
+    } else {
+      scrollTopBtn.classList.remove("show");
+    }
+  });
+}
+
+
+/*===============================================
+  5. Sliders
+===============================================*/
+//
+// Portfolio Slider //
+//
+var swiper = new Swiper(".portfolio-slider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-portfolio-next",
+    prevEl: ".swiper-portfolio-prev",
+  },
+});
+
+//
+// Blog Slider //
+//
+var swiper = new Swiper(".blog-slider", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 24,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-blog-next",
+    prevEl: ".swiper-blog-prev",
+  },
+});
+
+//
+// Clients Slider //
+//
+var swiper = new Swiper(".clients-slider", {
+  slidesPerView: 2,
+  spaceBetween: 24,
+  breakpoints: {
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
+
+//
+// Testimonial Slider //
+//
+var swiper = new Swiper(".testimonial-slider", {
+  slidesPerView: 1,
+  spaceBetween: 40,
+  pagination: {
+    el: ".swiper-testimonial-pagination",
+    type: "progressbar",
+  },
+});
+
+
+/*===============================================
+  6. Lightbox
+===============================================*/
+//
+// Lightbox - Image //
+//
+var $lightboxImage = $(".lightbox-image-box");
+
+$lightboxImage.each(function () {
+  var $this = $(this);
+  $this.magnificPopup({
+    type: 'image',
+    fixedContentPos: false,
+    removalDelay: 200,
+    closeOnContentClick: true, 
+    image: {
+      titleSrc: 'data-image-title'
+    }
+  });
+});
+
+//
+// Lightbox - Media //
+//
+var $lightboxMedia = $(".lightbox-media-box");
+
+$lightboxMedia.each(function() {
+  var $this = $(this);
+  $this.magnificPopup({
+    type: "iframe",
+    fixedContentPos: false,
+    removalDelay: 200,
+    preloader: false,
+    iframe: {
+      patterns: {
+        youtube: {
+          index: 'youtube.com/',
+          id: 'v=',
+          src: '//www.youtube.com/embed/%id%?autoplay=1&rel=0'
+        },
+          vimeo: {
+          index: 'vimeo.com/',
+          id: '/',
+          src: '//player.vimeo.com/video/%id%?autoplay=1'
+        }
+      },
+      srcAction: "iframe_src" 
+    }
+  });
+});
+
+
+/*===============================================
+  7. Google Maps
+===============================================*/
+var mapCanvas = $(".gmap");
+
+if (mapCanvas.length) {
+  var m,divId,initLatitude, initLongitude, map;
+
+  for (var i = 0; i < mapCanvas.length; i++) {
+    m = mapCanvas[i];
+
+    initLatitude = m.dataset["latitude"];
+    initLongitude = m.dataset["longitude"];
+    divId = "#"+ m["id"];
+
+    map = new GMaps({
+      el: divId,
+      lat: initLatitude,
+      lng: initLongitude,
+      zoom: 16,
+      scrollwheel: false,
+      styles: [
+          /* style your map at https://snazzymaps.com/editor and paste JSON here */
+      ]
+    });
+
+    map.addMarker({
+      lat : initLatitude,
+      lng : initLongitude
+    });
+  }
+}
+
+
+/*===============================================
+  8. Contact Form
+===============================================*/
+$("#contactform").on("submit", function(e) {
+  var name = $("#name").val();
+  var email = $("#email").val();
+  var subject = $("#subject").val();
+  var message = $("#message").val();
+
+  if (name === "") {
+    $("#name").addClass("error-color");
+  }
+  if (email === "") {
+    $("#email").addClass("error-color");
+  }
+  if (subject === "") {
+    $("#subject").addClass("error-color");
+  }
+  if (message === "") {
+    $("#message").addClass("error-color");
+  }
+
+  else {
+    $.ajax({
+      url:"assets/php/contact-form.php",
+      data:$(this).serialize(),
+      type:"POST",
+      success:function(data){
+        $("#success").addClass("show-result"); //=== Show Success Message==
+        $("#contactform").each(function(){
+          this.reset();
+        });
+      },
+      error:function(data){
+        $("#error").addClass("show-result"); //===Show Error Message====
+      }
+    });
+    var forms = $("#contactform input, #contactform textarea");
+    forms.removeClass("error-color");
+  }
+
+  e.preventDefault();
+});
