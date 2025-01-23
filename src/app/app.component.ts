7c814c9856fb85796b6c995bdf3c3919dc2e96d4
Yahya Akli <yahyaakli@Yahyas-MBP.lan>
2025-01-23 16:00:11 -0500

    Knowledge graph

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ServicesComponent } from './components/services/services.component';
+import { KnowledgeGraphComponent } from './components/knowledge-graph/knowledge-graph.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutUsComponent, ServicesComponent],
+  imports: [
+    RouterOutlet, 
+    AboutUsComponent, 
+    ServicesComponent,
+    KnowledgeGraphComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/knowledge-graph/knowledge-graph.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-knowledge-graph',
+  standalone: true,
+  imports: [],
+  templateUrl: './knowledge-graph.component.html',
+  styleUrl: './knowledge-graph.component.scss'
+})
+export class KnowledgeGraphComponent {
+
+}
