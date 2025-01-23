5ba80e59d8f6edba80104f09062c59b204ed1c57
Yahya Akli <yahyaakli@Yahyas-MBP.lan>
2025-01-23 15:47:17 -0500

    Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ServicesComponent } from './components/services/services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutUsComponent],
+  imports: [RouterOutlet, AboutUsComponent, ServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
